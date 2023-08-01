ca2eabbc5615039ec70d954c1e883d5345904402
DucAnh2002 <haducanh02012002@gmail.com>
2023-08-01 22:01:55 +0700

    update

---
+++ script.js
@@ -0,0 +1,7 @@
+const adressbtn = document.querySelector('#adress-form')
+// console.log(adressbtn)
+adressbtn.addEventListener("click", function(){
+    document.querySelector('.adress-form').style.display = "flex";
+})
+
+
